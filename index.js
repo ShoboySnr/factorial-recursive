2e86f88fab153d30320a373564291ef4a838416e
Damilare <48989517+ShoboySnr@users.noreply.github.com>
2019-11-22 11:39:52 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function FirstFactorial(num) { 
+
+  if (num === 0)
+  {
+    return 1;
+  }
+  else {
+    return num * FirstFactorial( num - 1);
+  } 
+}
+
+console.log(FirstFactorial(readline()));
